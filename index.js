0a56744b80bd6a8e05a6e414a3ebb0adf3554470
Brooke Heck <b19heck@gmail.com>
2022-10-14 03:27:58 +0000

    update readme

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 'use strict';
 
 // importing vendor messages and driver messages will start the subscription to queues on AWS
-// const vendorMesages = require('./vendor/vendorMessages');
+// const vendorMessages = require('./vendor/vendorMessages');
 // const driverMessages = require('./driver/driverMessages');
 
 // starts the chain of events by alerting publishing to SNS topic on AWS
 const publishPickup = require('./vendor/publishPickup');
 
-setInterval(() => {
+let count = 5;
+const intervalId = setInterval(() => {
   publishPickup();
-}, 5000);
+  count--;
+  if(count === 0) clearInterval(intervalId);
+}, 3000);
 
 
